79fab38f0dc2dc1e0c0ff7180cc8a504f47d6161
yyqdataa <yyq18907177621@163.com>
2016-06-27 10:25:36 +0800

    初始导入

---
+++ app/src/main/java/com/phone1000/app/androidtop/MainActivity.java
@@ -0,0 +1,13 @@
+package com.phone1000.app.androidtop;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
